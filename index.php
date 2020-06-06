b51653b86d2cc20a7cfac8473413edf7ac7f6cdb
root <root@raspberrypi>
2020-06-06 18:20:30 +0100

    update

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php include_once("home.html"); ?>
+<?php include_once("home.html"); 
+$servername = "uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "y83zbvzpsmhkv9u1";
+$password = "p4nlaaog7kn86ajf";
+$dbname = "c6gg8gz3f616n9de";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM questions";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "$row";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
