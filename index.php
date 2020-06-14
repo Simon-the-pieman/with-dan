b0c903afe1dd18c84622a6f4cfec1578971fb7a6
root <root@raspberrypi>
2020-06-14 12:16:32 +0100

    connect to database

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "$row";
+    echo "$row[question]";
   }
 } else {
   echo "0 results";
