5d1cb58fad1447efefb0a9b712a3fd6582b44b4c
root <root@raspberrypi>
2020-06-21 16:58:17 +0100

    fixing php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-echo "$row[question]";
+echo "$row[question]<br/>";
 
     echo "$row[correct_answer]<br/>";
    
