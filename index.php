734b88ce6ddd239761914439515424a0b6fea230
root <root@raspberrypi>
2020-06-21 17:17:03 +0100

    adding script

--- index.php
+++ index.php
@@ -18,12 +18,12 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 echo "$row[question]<br/>";
-echo "<script>";
- document.write($row[correct_answer]);   
+
+    echo "$row[correct_answer]<br/>";
     echo "$row[answer_2]<br/>";
     echo "$row[answer_3]<br/>";
     echo "$row[answer_4]<br/>";
-echo "</script>";
+
     echo "<br/>";
 }
 } else {
