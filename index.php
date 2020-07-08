f69ee33c57d6b4b908fb8a35ef335089247313a9
Simon-the-pieman <66211006+Simon-the-pieman@users.noreply.github.com>
2020-07-08 20:35:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,10 +17,9 @@ echo"<script type='application/javascript'>";
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-echo "$row[question]<br/>";
+print ($row[question]<br/>);
 
-    echo "$row[correct_answer]";
-    <br/>;
+    echo "$row[correct_answer]<br/>";
     echo "$row[answer_2]<br/>";
     echo "$row[answer_3]<br/>";
     echo "$row[answer_4]<br/>";
