ef8a927e8989418680e7db58acb57c28537d7f3a
Simon-the-pieman <66211006+Simon-the-pieman@users.noreply.github.com>
2020-07-08 20:44:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ $servername = "uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
 $username = "y83zbvzpsmhkv9u1";
 $password = "p4nlaaog7kn86ajf";
 $dbname = "c6gg8gz3f616n9de";
-
+// create function
+echo "function checkifright {alert ("alert")}";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -18,11 +19,11 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 echo "$row[question]<br/>";
-
-    echo "$row[correct_answer]<br/>";
-    echo "$row[answer_2]<br/>";
-    echo "$row[answer_3]<br/>";
-    echo "$row[answer_4]<br/>";
+    
+    echo "$row[correct_answer]<br/>" onclick='checkifright()';
+    echo "$row[answer_2]<br/>" onclick='checkifright()';
+    echo "$row[answer_3]<br/>" onclick='checkifright()';
+    echo "$row[answer_4]<br/>" onclick='checkifright()';
 
     echo "<br/>";
 }
