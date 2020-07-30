5b879f308b63813adbc281c0dee9d26ee0861c2b
Simon-the-pieman <66211006+Simon-the-pieman@users.noreply.github.com>
2020-07-30 21:17:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     echo "<form>";
     echo "$row[question]";
-    echo '<input type="radio" id="answer" name="answer" value="$row[correct_answer]"> $row[correct_answer]<br/>';
+    echo '<input type="radio" id="answer" name="answer" value="$row[correct_answer]"> <label for="answer">$row[correct_answer]</label><br/>';
     echo "</form>";
     echo "$row[answer_2]<br/>";
     echo "$row[answer_3]<br/>";
